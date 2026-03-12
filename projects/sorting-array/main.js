965107eefee4623f5e862549ac399b714ae82799
Huynh Khanh Hung <90737345+hung0725@users.noreply.github.com>
2026-03-12 22:36:27 +0700

    Refactor sorting algorithms and enhance UI interactions

--- projects/sorting-array/main.js
+++ projects/sorting-array/main.js
@@ -11,6 +11,10 @@ function sort(ar){
     return ar;
 }
 
+var methodbtn = document.getElementById('methods');
+methodbtn.oninput = () => {
+
+}
 
 let autoEv;
 var autoBtn = document.getElementById('auto');
@@ -22,8 +26,15 @@ function autoclick(itv){
     autoBtn.style.display = 'none';
 }
 function cont(){
-    bubbleSort(a);
-    
+    switch(parseInt(methodbtn.value)){
+        case 0:
+            bubbleSort(a);
+            break;
+        case 1:
+            selSort(a);
+            break;
+        
+    }    
 }
 function stopauto(){
     clearInterval(autoEv);
@@ -59,17 +70,22 @@ function displayArray(ar){
 
 var colors = [
     'green',
-    'blue'
+    'blue',
+    'checked'
 ]
-function focusEl(ind, colorInd){
+// x is the number of element to highlight
+function focusEl(ind, colorInd, x){
     Array.from(arrayEl.children).forEach(el => {
         el.classList.remove(colors[colorInd]);
     });
-    arrayEl.children[ind].classList.add(colors[colorInd]);
     Array.from(arrayInd.children).forEach(el => {
         el.classList.remove(colors[colorInd]);
     });
-    arrayInd.children[ind].classList.add(colors[colorInd]);
+    
+    for(var i = ind; i < x + ind; i++){
+        arrayInd.children[i].classList.add(colors[colorInd]);
+        arrayEl.children[i].classList.add(colors[colorInd]);
+    }
 }
 function swapEl(i, j){
     var v1 = arrayEl.children[i].innerText,
@@ -98,8 +114,8 @@ function bubbleSort(ar){
         return;
     }
     displayVar(['i', 'j', 'swapped'],[i, j, swapped], [null, null, boolClass[swapped ? 1 : 0]]);
-    focusEl(j, 0);
-    focusEl(j + 1, 1);
+    focusEl(j, 0, 2, 1);
+    focusEl(j + 1, 1, 1);
     if(clk){
         if(ar[j] > ar[j + 1]){
             alt(`a[j] > a[j+1] ${ar[i]} > ${ar[j]}, swapping`, 2);
@@ -130,6 +146,7 @@ function bubbleSort(ar){
     
 }
 
+// display variable name and value in HTML page
 var vlist = document.getElementsByClassName('var-list')[0];
 function displayVar(vars, value, classes){
     vlist.replaceChildren();
@@ -143,6 +160,8 @@ function displayVar(vars, value, classes){
 
     vlist.appendChild(fragment);
 }
+// used when the sorting is done
+// clear interval, display text
 function sortDone(){
     alt('sorting done', 3);
     swapDone(ti, tj);
@@ -156,8 +175,15 @@ var altCls = [
     'swap',
     'end'
 ]
-function alt(txt, cls){
-    altEl.innerText = txt;
+
+
+// render a text, add class to a DOM element
+function alt(txt, cls, isxml){
+    if(isxml){
+        altEl.innerHTML = txt;
+    } else {
+        altEl.innerText = txt;
+    }
     altCls.forEach(i => {
         altEl.classList.remove(i);
     });
@@ -174,11 +200,111 @@ function rnd(){
     displayArray(a);
 }
 
-displayArray(a);
 
 
-function selSort(ar){
+
+// selection sort algorithm
+function selalg(ar){
     for(var i = 0; i < ar.length; i++){
-        
+        var m = i;
+        for(var j = i + 1; i < ar.length; i++){
+            if(ar[j] < a[m]){
+                m = i;
+            }
+        }
+        [ar[i], ar[m]] = [ar[m], ar[i]];
     }
 }
+
+var sel = false, jmin = 0;
+// isloop: used this so user have 2 click 2 times to continue to loop
+// one of the click initialize iterate value (i, j), the other one display the variable in middle of the loop
+var isloop = false;
+// tmpch: check if j loop has ended to rerender j_min DOM
+var tmpch = false, jend = false;
+function selSort(ar){
+    // condition to stop when the sorting is done
+    if(i >= ar.length - 1){
+        swapDone(ti, tj);
+        sortDone();
+        console.log('done');
+        return;
+    }
+    // initializing selection sort (just once)
+    if(!sel){
+        sel = true;
+        i = 0;
+        j = i + 1;
+        markEl(i);
+    }
+    // highlight the DOM
+    focusEl(i, 0, 1);
+    focusEl(j, 1, 1);
+    alt("Continue loop");
+
+    // isloop value will be toggled between 0 and 1 on click
+    // that lead to different behaviours inside this function
+    // isloop value: 0 - 1 - 0 - 1 -...
+    if(isloop){
+        // that is what run inside the j loop
+        if(ar[j] < ar[jmin]){
+            alt(`a[${j}] < a[${jmin}] so j_min = ${j}`, 2);
+            jmin = j;
+            markEl(jmin);
+        }
+        isloop = !isloop;
+    } else {
+        // when the loop interate, remove previous display swapEl
+        isloop = !isloop;
+        swapDone(ti, tj);
+        // if j loop end, i value +1, highlight jmin at i (initialize jmin)
+        if(tmpch){
+            markEl(i);
+            console.log(tmpch);
+            tmpch = false;
+        }
+    }
+
+    // display variables in HTML
+    displayVar(['i', 'j','j_min'],[i, j, jmin],[]);
+
+    // when the comparison is done, its time to increase j (and i) value
+    if(!isloop){
+        j++;
+        // increase i by 1 and continue the loop, if the j loop end
+        if(j >= ar.length){
+            // render swap el
+            swapEl(i, jmin);
+            
+            // render alt text
+            if(altEl.innerText[0] == 'a'){
+                alt(`a[${j-1}] < a[${jmin}] so j_min = ${j-1}\n<i>Swap a[${i}] and a[${jmin}]</i>`, 2, 1);
+            } else {
+                alt(`<i>Sort a[${i}] and a[${jmin}]</i>`, 2, 1);
+            }
+            // temporary variables, used for swapDone()
+            ti = i;
+            tj = jmin;
+            // swap array el
+            [ar[i], ar[jmin]] = [ar[jmin], ar[i]];
+            // increase i value and initialize variables
+            i++;
+            j = i + 1;
+            jmin = i;
+            tmpch = true;
+            jend = false;
+        }
+    }
+}
+
+// mark array DOM element with red underline
+function markEl(ind){
+    Array.from(arrayEl.children).forEach(el => {
+        el.classList.remove('mark-el');
+    });
+    arrayEl.children[ind].classList.add('mark-el');
+}
+
+
+
+displayArray(a);
